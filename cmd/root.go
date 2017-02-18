a9c4efbe5517081ba437f5c4e779e644886e7bba
Nick Klauer <klauer@gmail.com>
2017-02-18 15:53:12 -0600

    fix root.go

--- cmd/root.go
+++ cmd/root.go
@@ -68,8 +68,7 @@ func initConfig() {
 
 	viper.SetConfigName(".printshop") // name of config file (without extension)
 	viper.AddConfigPath("$HOME")      // adding home directory as first search path
-	viper.Add
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv()              // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
