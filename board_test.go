f921e1d1cc4f7b464fe914969c9b52b2e1f4425c
Nick Klauer <klauer@gmail.com>
2017-02-19 10:48:58 -0600

    restart the test setup with boards

--- board_test.go
+++ board_test.go
@@ -1,65 +1,129 @@
 package printshop
 
 import (
-	"os"
 	"reflect"
+	"sync"
 	"testing"
 
-	"github.com/VojtechVitek/go-trello"
-	"github.com/go-check/check"
-)
+	"os"
 
-func Test(t *testing.T) {
-	check.TestingT(t)
-}
+	"github.com/VojtechVitek/go-trello"
+	"gopkg.in/check.v1"
+)
 
 type BoardSuite struct {
 	client *trello.Client
 }
 
+func TestWithCheck(t *testing.T) {
+	check.TestingT(t)
+}
+
 var _ = check.Suite(&BoardSuite{})
 
-func (b *BoardSuite) NewTrelloBoard() *trello.Board {
-	return &trello.Board{}
-}
-
-func (b *BoardSuite) NewTrelloCard(id string) *trello.Card {
-	const DefaultCardID = ""
-	if id == "" {
-		card, err := b.client.Card(DefaultCardID)
-		if err != nil {
-			panic(err)
-		}
-		return card
-	}
-	card, err := b.client.Card(id)
-	if err != nil {
-		panic(err)
-	}
-	return card
-}
-
-func (b *BoardSuite) NewTrelloList() *trello.List {
-	return &trello.List{}
-}
-
-func (b *BoardSuite) SetUpSuite(c *check.C) {
-	key := os.Getenv(KEY_ENV)
+func (s *BoardSuite) SetUpSuite(c *check.C) {
 	token := os.Getenv(TOKEN_ENV)
+	key := os.Getenv(KEY_ENV)
 	client, err := trello.NewAuthClient(key, &token)
 	if err != nil {
 		panic(err)
 	}
-	b.client = client
+	s.client = client
 }
 
-var testSuite *BoardSuite
-
-func init() {
-	testSuite = &BoardSuite{}
+func (s *BoardSuite) TearDownSuite(c *check.C) {
 }
 
-func (b *BoardSuite) TestNewEmail(t *check.C) {
+func (s *BoardSuite) TestNewContainer(c *check.C) {
+	type args struct {
+		c       *trello.Client
+		boardID string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *BoardContainer
+		wantErr bool
+	}{
+		{
+			"generic",
+			args{
+				c:       s.client,
+				boardID: "5893eac416340d068cb41a2b",
+			},
+			&BoardContainer{},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		got, err := NewContainer(tt.args.c, tt.args.boardID)
+		c.Check(got, check.NotNil)
+		c.Check(err, check.IsNil)
+	}
+}
+
+func TestBoardContainer_RetrieveCards(t *testing.T) {
+	type fields struct {
+		board *trello.Board
+		lists []*trello.List
+		cards map[listID][]trello.Card
+		mux   sync.Mutex
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &BoardContainer{
+				board: tt.fields.board,
+				lists: tt.fields.lists,
+				cards: tt.fields.cards,
+				mux:   tt.fields.mux,
+			}
+			if err := c.RetrieveCards(); (err != nil) != tt.wantErr {
+				t.Errorf("BoardContainer.RetrieveCards() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestBoardContainer_ListWorker(t *testing.T) {
+	type fields struct {
+		board *trello.Board
+		lists []*trello.List
+		cards map[listID][]trello.Card
+		mux   sync.Mutex
+	}
+	type args struct {
+		id      int
+		listJob <-chan *trello.List
+		results chan<- []trello.Card
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &BoardContainer{
+				board: tt.fields.board,
+				lists: tt.fields.lists,
+				cards: tt.fields.cards,
+				mux:   tt.fields.mux,
+			}
+			c.ListWorker(tt.args.id, tt.args.listJob, tt.args.results)
+		})
+	}
+}
+
+func TestNewEmail(t *testing.T) {
 	type args struct {
 		b *trello.Board
 	}
@@ -68,16 +132,14 @@ func (b *BoardSuite) TestNewEmail(t *check.C) {
 		args args
 		want *Email
 	}{
-		{
-			"generic",
-			args{
-				b: b.NewTrelloBoard(),
-			},
-			&Email{},
-		},
+	// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		t.Check(NewEmail(tt.args.b), check.DeepEquals, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewEmail(tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewEmail() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
@@ -91,14 +153,7 @@ func TestNewMetaData(t *testing.T) {
 		want    *MetaData
 		wantErr bool
 	}{
-		{
-			"nil, empty",
-			args{
-				&trello.List{},
-			},
-			&MetaData{},
-			false,
-		},
+	// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -114,7 +169,7 @@ func TestNewMetaData(t *testing.T) {
 	}
 }
 
-func (b *BoardSuite) TestNewSection(t *check.C) {
+func TestNewSection(t *testing.T) {
 	type args struct {
 		l *trello.List
 	}
@@ -124,23 +179,23 @@ func (b *BoardSuite) TestNewSection(t *check.C) {
 		want    *Section
 		wantErr bool
 	}{
-		{
-			"generic",
-			args{
-				b.NewTrelloList(),
-			},
-			&Section{},
-			false,
-		},
+	// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		got, err := NewSection(tt.args.l)
-		t.Check(got, check.DeepEquals, tt.want)
-		t.Check(err, check.IsNil)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewSection(tt.args.l)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewSection() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewSection() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func (b *BoardSuite) TestNewArticle(t *check.C) {
+func TestNewArticle(t *testing.T) {
 	type args struct {
 		card *trello.Card
 	}
@@ -150,18 +205,18 @@ func (b *BoardSuite) TestNewArticle(t *check.C) {
 		want    *Article
 		wantErr bool
 	}{
-		{
-			"generic",
-			args{
-				card: b.NewTrelloCard(""),
-			},
-			&Article{},
-			false,
-		},
+	// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		got, err := NewArticle(tt.args.card)
-		t.Check(err, check.IsNil)
-		t.Check(got, check.DeepEquals, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewArticle(tt.args.card)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewArticle() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewArticle() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
